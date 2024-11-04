6487dda7ec2f7069aad03e5424e347bc9a95b57e
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2024-11-04 13:32:48 +0100

    diaporama

--- script/script.js
+++ script/script.js
@@ -8,17 +8,15 @@ const imgBurgerName = [
 let index = 0;
 
 function changeDiaporamaBurgerImg() {
-    console.log(index);
 
-    path = imgBurgerName[index];
-    diaporamaBurgerImg.setAttribute("src", "/images/burger/" + path + ".jpeg");
+    let path = imgBurgerName[index];
+    diaporamaBurgerImg.setAttribute("src", "images/burger/" + path + ".jpeg");
 
     if ((index + 1) == imgBurgerName.length) {
         index = 0;
     } else {
         index++;
     }
-    console.log(index, imgBurgerName.length);
 }
 
-setInterval(changeDiaporamaBurgerImg, 1000);
+setInterval(changeDiaporamaBurgerImg, 10000);
