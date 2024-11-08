b77227efe4803252aec017a00c31df5241781ebf
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2024-11-08 17:21:19 +0100

    text

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 const diaporamaBurgerImg = document.getElementById("diaporamaBurgerImg");
+const burgerName = document.getElementById("burgerName");
+
 
 const imgBurgerName = [
     "burger_naturel",
@@ -12,6 +14,8 @@ function changeDiaporamaBurgerImg() {
     let path = imgBurgerName[index];
     diaporamaBurgerImg.setAttribute("src", "images/burger/" + path + ".jpeg");
 
+    getBurgerName(path);
+
     if ((index + 1) == imgBurgerName.length) {
         index = 0;
     } else {
@@ -19,4 +23,19 @@ function changeDiaporamaBurgerImg() {
     }
 }
 
+diaporamaBurgerImg.onclick = function () {
+    if (burgerName.getAttribute("style") == "display: none;") {
+        console.log(burgerName.getAttribute("style"));
+        burgerName.setAttribute("style", "display: flex;");
+        burgerName.textContent(getBurgerName());
+    } else {
+        burgerName.setAttribute("style", "display: none;");
+    }
+};
+
+function getBurgerName(path) {
+    return path.split("_");
+}
+
 setInterval(changeDiaporamaBurgerImg, 1000);
+
