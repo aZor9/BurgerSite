b7e78a65eba4bf9c5b89b182f1ee7be5a7ba34d4
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2024-11-15 14:59:57 +0100

    api.js

--- script/api.js
+++ script/api.js
@@ -1,15 +1,35 @@
+const urlApi = "https://opendata.agencebio.org/api/gouv/operateurs/?siret=79317749400028";
+
+
 function getData() {
     let request = new XMLHttpRequest();
     request.onreadystatechange = stockData;
-    request.open('GET', 'https://opendata.agencebio.org/api/gouv/operateurs/');
-    // request.open('GET', 'https://data.economie.gouv.fr/documentation/api/v2/catalog/facets');
+    request.open('GET', urlApi);
     request.send();
 }
 
 function stockData() {
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        console.log(JSON.parse(this.responseText));  // Correction ici
+        const $data = JSON.parse(this.responseText)
+        showData($data);
     }
+}
+
+function showData($data) {
+    const numeroBio = $data["items"][0]["numeroBio"];
+    const gerant = $data["items"][0]["gerant"];
+    const lieu = $data["items"][0]["adressesOperateurs"][0]["lieu"];
+    const codePostal = $data["items"][0]["adressesOperateurs"][0]["codePostal"];
+    const ville = $data["items"][0]["adressesOperateurs"][0]["ville"];
+
+    const productions = $data["items"][0]["productions"];
+    let productionsNom = [];
+
+    productions.forEach(element => {
+        productionsNom.push(element.nom);
+    });
+
+    console.log(numeroBio, gerant, lieu, codePostal, ville, productionsNom);
 }
 
 getData();
