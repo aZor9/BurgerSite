ede12e1660b6a278b762be949893c1bf75bc3e21
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2024-11-15 14:58:25 +0100

    test.js

--- script/create_burger.js
+++ script/create_burger.js
@@ -1,11 +1,11 @@
 // Initialisation de la Map
-let listeIngredients = new Map(); 
-if (window.localStorage.getItem("listeIngredients")) { 
+let listeIngredients = new Map();
+if (window.localStorage.getItem("listeIngredients")) {
   const savedIngredients = window.localStorage.getItem("listeIngredients"); // Récupérer la Map depuis le localStorage
   listeIngredients = new Map(JSON.parse(savedIngredients)); // Convertir la chaîne JSON en array, puis en Map  
   window.localStorage.setItem("listeIngredients", JSON.stringify(Array.from(listeIngredients.entries()))); // Convertir la Map en array, puis en chaîne JSON et la restocker dans le localStorage    
 } else {
-    console.log("Aucun ingrédient n'a été enregistré");
+  console.log("Aucun ingrédient n'a été enregistré");
 }
 
 const ingredient1 = document.getElementById("ingredient1");
@@ -13,10 +13,10 @@ const ingredient2 = document.getElementById("ingredient2");
 const ingredient3 = document.getElementById("ingredient3");
 
 // Liste des burgers
-let listeBurgers = new Map(); 
-let ingredientBurger = []; 
-if (window.localStorage.getItem("listeBurgers")) { 
-  const savedBurgers = window.localStorage.getItem("listeBurgers"); 
+let listeBurgers = new Map();
+let ingredientBurger = [];
+if (window.localStorage.getItem("listeBurgers")) {
+  const savedBurgers = window.localStorage.getItem("listeBurgers");
   listeBurgers = new Map(JSON.parse(savedBurgers)); // Convertir la chaîne JSON en array, puis en Map
   window.localStorage.setItem("listeBurgers", JSON.stringify(Array.from(listeBurgers.entries()))); // Convertir la Map en array, puis en chaîne JSON et la stocker dans le localStorage
 
@@ -28,7 +28,7 @@ if (window.localStorage.getItem("listeBurgers")) {
     nameCell.textContent = nomBurger;
     row.appendChild(nameCell);
     const ingredientsCell = document.createElement("td");
-    ingredientsCell.textContent = ingredients.join(" + "); 
+    ingredientsCell.textContent = ingredients.join(" + ");
     row.appendChild(ingredientsCell);
     burgerTableBody.appendChild(row);
   });
@@ -38,10 +38,10 @@ if (window.localStorage.getItem("listeBurgers")) {
 // affichage ingredient 
 function affichageIngredient(numIngredient) {
   listeIngredients.forEach((quantite, nom) => {
-    const option = document.createElement("option"); 
+    const option = document.createElement("option");
     option.value = nom;
-    option.textContent = `${quantite} - ${nom}  `;      
-    numIngredient.appendChild(option);                  
+    option.textContent = `${quantite} - ${nom}  `;
+    numIngredient.appendChild(option);
   });
 }
 affichageIngredient(ingredient1);
@@ -50,13 +50,13 @@ affichageIngredient(ingredient3);
 
 
 // création du burger
-document.getElementById("creationBurger").addEventListener("submit", function(event) {
+document.getElementById("creationBurger").addEventListener("submit", function (event) {
   event.preventDefault(); // empêche l'envoi du formulaire pour pouvoir traiter les données
 
   // test et verrouillage
   var noErrorQuantity = true; // true = pas d'erreur
   var listeIngredientsTestNoErrorQuantity = new Map(listeIngredients); // Crée une copie de la Map listeIngredients pour tester les quantités
-  
+
   const selectedIngredient1 = ingredient1.value;
   const selectedIngredient2 = ingredient2.value;
   const selectedIngredient3 = ingredient3.value;
@@ -67,13 +67,13 @@ document.getElementById("creationBurger").addEventListener("submit", function(ev
   function checkQuantity(selectedIngredient) {
     if (listeIngredientsTestNoErrorQuantity.get(selectedIngredient) > 0) {
       listeIngredientsTestNoErrorQuantity.set(selectedIngredient, listeIngredientsTestNoErrorQuantity.get(selectedIngredient) - 1);
-    } else  {
+    } else {
       noErrorQuantity = false;
     }
   }
   checkQuantity(selectedIngredient1);
   checkQuantity(selectedIngredient2);
-  checkQuantity(selectedIngredient3);  
+  checkQuantity(selectedIngredient3);
 
   // console.log(listeIngredientsTestNoErrorQuantity);
 
@@ -82,7 +82,7 @@ document.getElementById("creationBurger").addEventListener("submit", function(ev
     // creation du burger 
     ingredientBurger = [selectedIngredient1, selectedIngredient2, selectedIngredient3];
     listeBurgers.set(document.getElementById("nomBurger").value, ingredientBurger);
-    window.localStorage.setItem("listeBurgers", JSON.stringify(Array.from(listeBurgers.entries()))); 
+    window.localStorage.setItem("listeBurgers", JSON.stringify(Array.from(listeBurgers.entries())));
 
     // modifier les quantités des ingrédients sur la vrai map
     listeIngredients.set(selectedIngredient1, listeIngredients.get(selectedIngredient1) - 1);
@@ -95,11 +95,11 @@ document.getElementById("creationBurger").addEventListener("submit", function(ev
         listeIngredients.delete(nom);
       }
     });
-      
+
     // Mettez à jour le localStorage avec les nouvelles quantités
     window.localStorage.setItem("listeIngredients", JSON.stringify(Array.from(listeIngredients.entries())));
-    
-    ErreurIngredient.style.display = "none";  
+
+    ErreurIngredient.style.display = "none";
     creationBurgerGood.style.display = "block";
     setTimeout(() => {
       creationBurgerGood.style.display = "none";
@@ -116,45 +116,45 @@ document.getElementById("creationBurger").addEventListener("submit", function(ev
 });
 
 // suppression des ingredient
-document.getElementById("supressionIngredient").addEventListener("submit", function(event) {
+document.getElementById("supressionIngredient").addEventListener("submit", function (event) {
   event.preventDefault();
   window.localStorage.removeItem("listeIngredients");
   listeIngredients.clear();
   console.log("Ingrédients supprimés");
   location.reload();
-  }
+}
 );
 
 // suppression des burger
-document.getElementById("resetListBurger").addEventListener("submit", function(event) {
+document.getElementById("resetListBurger").addEventListener("submit", function (event) {
   event.preventDefault();
   window.localStorage.removeItem("listeBurgers");
   listeBurgers.clear();
   console.log("Burgers supprimés");
   location.reload();
-  }
+}
 );
 
 // reaprovisionnement des ingredient
-document.getElementById("InitialisationIngredient").addEventListener("submit", function(event) {
-  listeIngredients.set("Salade", 10); 
+document.getElementById("InitialisationIngredient").addEventListener("submit", function (event) {
+  listeIngredients.set("Salade", 10);
   listeIngredients.set("Tomate", 5);
   listeIngredients.set("Fromage", 2);
-  listeIngredients.set("Steak", 1);  
-  window.localStorage.setItem("listeIngredients", JSON.stringify(Array.from(listeIngredients.entries()))); 
+  listeIngredients.set("Steak", 1);
+  window.localStorage.setItem("listeIngredients", JSON.stringify(Array.from(listeIngredients.entries())));
   console.log("Ingrédients réapprovisionnés");
-  }
+}
 );
 
 //mauvais nom de burger
-var nomBurger = document.getElementById("nomBurger"); 
-nomBurger.addEventListener("keyup", function (event) { 
-    var nomValue = nomBurger.value.trim(); // associe la variable nomValue à la valeur de nomBurger sans les espaces
-    if (/\d/.test(nomValue)) { 
-        ErreurBurgerNom.style.display = "block"; // affiche le message d'erreur
-        noErrorName = false;
-    } else {
-      ErreurBurgerNom.style.display = "none"; // masque le message d'erreur
-        noErrorName = true;
-    }
+var nomBurger = document.getElementById("nomBurger");
+nomBurger.addEventListener("keyup", function (event) {
+  var nomValue = nomBurger.value.trim(); // associe la variable nomValue à la valeur de nomBurger sans les espaces
+  if (/\d/.test(nomValue)) {
+    ErreurBurgerNom.style.display = "block"; // affiche le message d'erreur
+    noErrorName = false;
+  } else {
+    ErreurBurgerNom.style.display = "none"; // masque le message d'erreur
+    noErrorName = true;
+  }
 });
